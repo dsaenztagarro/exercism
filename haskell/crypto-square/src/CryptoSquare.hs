cd6bd951b1b563583c7dc82df597e6c26348a868
David Saenz <dsaenz@bebanjo.com>
2018-11-18 08:45:09 +0100

    Haskell: Crypto Square (best solution)

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,27 +1,12 @@
 module CryptoSquare (encode) where
 
-import           Data.Char (isLetter, isNumber, toLower)
+import           Data.Char       (isAlphaNum, toLower)
+import           Data.List       (transpose)
+import           Data.List.Split (chunksOf)
 
 encode :: String -> String
-encode xs = unwords $ map getRow [0..y-1]
-  where text = normalize xs
-        (x, y) = dimensions text
-        getRow i = spacesUntil x $ getRowChars text y i
-
-normalize :: String -> String
-normalize = foldr step []
-  where step x xs | isLetter x = toLower x : xs
-                  | isNumber x = x : xs
-                  | otherwise = xs
-
-dimensions :: String -> (Int, Int)
-dimensions xs = (floor val, ceiling val)
-  where val = sqrt $ fromIntegral (length xs) :: Float
-
-spacesUntil :: Int -> String -> String
-spacesUntil n xs = xs ++ replicate (n - length xs) ' '
-
-getRowChars :: String -> Int -> Int -> String
-getRowChars text numRows numRow = map fst $ filter belongsToRow charsWithIndex
-  where charsWithIndex = zip text [0..]
-        belongsToRow (_,i) = (i - numRow) `mod` numRows == 0
+encode xs = unwords . transpose $ chunksOf sqSize cleaned
+  where cleaned = filter isAlphaNum . map toLower $ xs
+        sqSize = ceiling doubleSqSize
+          where doubleSqSize :: Double
+                doubleSqSize = sqrt . fromIntegral $ length cleaned
