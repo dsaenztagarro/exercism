4f15b252e0a7780212ed8fe92cbc913348321ce4
David Saenz <dsaenz@bebanjo.com>
2018-11-25 19:05:39 +0100

    Haskell: Bob (OverloadedStrings)

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,16 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
 import           Data.Char (isLetter)
 import qualified Data.Text as T
 
-responseFor :: String -> String
+responseFor :: T.Text -> T.Text
 responseFor xs
   | isSilence             = "Fine. Be that way!"
   | isQuestion && isShout = "Calm down, I know what I'm doing!"
   | isQuestion            = "Sure."
   | isShout               = "Whoa, chill out!"
   | otherwise             = "Whatever."
-  where cleaned    = T.strip $ T.pack xs
+  where cleaned    = T.strip xs
         isSilence  = T.null cleaned
-        isQuestion = T.isSuffixOf (T.pack "?") cleaned
+        isQuestion = T.isSuffixOf "?" cleaned
         isShout    = (T.any isLetter cleaned) && (T.toUpper cleaned) == cleaned
