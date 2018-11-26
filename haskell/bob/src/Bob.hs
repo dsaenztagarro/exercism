34caa41f4f1c3e7863d8e6df14f29e1ace6aada0
David Saenz <dsaenz@bebanjo.com>
2018-11-26 17:52:20 +0100

    Haskell: Bob (Code review)

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,17 +2,17 @@
 
 module Bob (responseFor) where
 
-import           Data.Char (isLetter)
+import           Data.Char (isLetter, isLower, isSpace)
+import           Data.Text (Text)
 import qualified Data.Text as T
 
-responseFor :: T.Text -> T.Text
+responseFor :: Text -> Text
 responseFor xs
   | isSilence             = "Fine. Be that way!"
   | isQuestion && isShout = "Calm down, I know what I'm doing!"
   | isQuestion            = "Sure."
   | isShout               = "Whoa, chill out!"
   | otherwise             = "Whatever."
-  where cleaned    = T.strip xs
-        isSilence  = T.null cleaned
-        isQuestion = T.isSuffixOf "?" cleaned
-        isShout    = (T.any isLetter cleaned) && (T.toUpper cleaned) == cleaned
+  where isSilence  = T.all isSpace xs
+        isQuestion = "?" `T.isSuffixOf` T.stripEnd xs
+        isShout    = T.any isLetter xs && T.all (not . isLower) xs
