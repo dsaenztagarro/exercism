72a32f23d3a211f9469dd4f563a2562ad4b003be
David Saenz <dsaenz@bebanjo.com>
2019-09-05 21:44:59 +0200

    Haskell: Collatz Conjecture (iteration)

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,11 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz x | x < 1 = Nothing
-          | otherwise = Just $ snd $ step (x, 0)
+collatz x | x > 0 = Just $ toInteger $ length (takeWhile (/= 1) $ iterate next x)
+          | otherwise = Nothing
 
-step :: (Integer, Integer) -> (Integer, Integer)
-step (1, n) = (1, n)
-step (x, n) = step (y, n + 1) where y = if even x
-                                        then x `div` 2
-                                        else 3 * x + 1
+next :: Integer -> Integer
+next x | even x = x `div` 2
+       | otherwise = 3 * x + 1
